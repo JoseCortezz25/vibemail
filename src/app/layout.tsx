16cae956cf5cff6e7be0d237635fdfed8e14a7f4
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-12-10 22:10:23 -0500

    refactor: migrate Google Analytics component to @next/third-parties and update integration in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
-import { GoogleAnalytics } from '@/domains/analytics/components/google-analytics';
 import { ReactNode } from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -41,7 +42,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <GoogleAnalytics />
+        <GoogleAnalytics
+          gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''}
+        />
         {children}
         <Toaster richColors />
       </body>
